1e9cf9c865fb4976478874f0f3c1904087c4b6b1
SolomonFoskaay <oskfak@gmail.com>
2024-03-05 00:17:25 +0000

    added react iframe to display substack sigunup emaillist form

--- app/page.tsx
+++ app/page.tsx
@@ -5,23 +5,25 @@ import { button as buttonStyles } from "@nextui-org/theme";
 import { siteConfig } from "@/config/site";
 import { title, subtitle } from "@/components/primitives";
 import { GithubIcon } from "@/components/icons";
+import Iframe from "react-iframe"; // import Iframe from react-iframe to render subscribe form
 
 export default function Home() {
 	return (
 		<section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
 			<div className="inline-block max-w-lg text-center justify-center">
-				<h1 className={title()}>Make&nbsp;</h1>
-				<h1 className={title({ color: "violet" })}>beautiful&nbsp;</h1>
+				<h1 className={title()}>Explore&nbsp;</h1>
+				<h1 className={title({ color: "violet" })}>Solana Ecosystem&nbsp;</h1>
 				<br />
 				<h1 className={title()}>
-					websites regardless of your design experience.
+					& 
+					<br/>Discover Opportunities!
 				</h1>
 				<h2 className={subtitle({ class: "mt-4" })}>
-					Beautiful, fast and modern React UI library.
+					Handpicked &quot;S&quot;, exclusive dApp updates, insights and opportunities await you:
 				</h2>
 			</div>
 
-			<div className="flex gap-3">
+			{/* <div className="flex gap-3">
 				<Link
 					isExternal
 					href={siteConfig.links.docs}
@@ -45,7 +47,16 @@ export default function Home() {
 						Get started by editing <Code color="primary">app/page.tsx</Code>
 					</span>
 				</Snippet>
-			</div>
+			</div> */}
+			<Iframe url="https://exploresol.substack.com/embed"
+        width="480px"
+        height="320px"
+        id="myId"
+        className="myClassname"
+        display="initial"
+        position="relative"
+        styles={{border: "1px solid #EEE", background: "white"}}
+      />
 		</section>
 	);
 }
