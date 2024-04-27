b38a3cb195036e4cc62e994ea888a4cc5b1b00a0
SolomonFoskaay <oskfak@gmail.com>
2024-04-27 08:59:42 +0000

    created EmailSubscriptionForm component

--- app/page.tsx
+++ app/page.tsx
@@ -6,8 +6,7 @@ import DirectoryPage from "@/components/DirectoryPage";
 import { ListingsNew } from "@/components/ListingsNew";
 import { ListingsUpcoming } from "@/components/ListingsUpcoming";
 import { ListingsLive } from "@/components/ListingsLive";
-import Iframe from "react-iframe"; // import Iframe from react-iframe to render subscribe form
-
+import EmailSubscriptionForm from "@/components/EmailSubscriptionForm";
 
 export default function HomePage() {
   // Animate words
@@ -33,37 +32,7 @@ export default function HomePage() {
   return (
     <div className="container mx-auto px-4">
       {/* Top text section with highlighted text */}
-      <div className="bg-black py-8">
-        <div className="text-center">
-          <h1 className="text-3xl md:text-4xl font-bold text-white mb-2">
-            Explore <span className="text-purple-400">Solana Ecosystem</span>
-          </h1>
-          <h2 className="text-xl md:text-2xl font-semibold text-white">
-            & Discover Opportunities!
-          </h2>
-          <p className="text-sm md:text-base text-purple-300 mt-2 px-4">
-            Handpicked <span className="text-pink-600">"S"</span>, exclusive
-            dApps/tools/resources updates, insights and opportunities await you:
-          </p>
-          <p>
-            <em>
-              Subscribe now and be part of the pioneering &quot;S&quot;
-              community.
-            </em>
-          </p>
-
-          <Iframe
-            url="https://exploresol.substack.com/embed"
-            width="480px"
-            height="320px"
-            id="myId"
-            className="myClassname"
-            display="initial"
-            position="relative"
-            styles={{ border: "1px solid #EEE", background: "white" }}
-          />
-        </div>
-      </div>
+      <EmailSubscriptionForm />
 
       {/* Directory Page component */}
       <div className="text-center">
