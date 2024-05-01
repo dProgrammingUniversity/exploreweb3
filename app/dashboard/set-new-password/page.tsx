5f72d979bf2c6b04ab2f69f843dcef03cc62f7a4
SolomonFoskaay <oskfak@gmail.com>
2024-05-01 19:47:42 +0000

    Adding Password Reset features 2

--- app/dashboard/set-new-password/page.tsx
+++ app/dashboard/set-new-password/page.tsx
@@ -1,17 +1,9 @@
 // ExploreSol/app/dashboard/set-new-password/page.tsx
-import { createClient } from "@/utils/supabase/server";
-import { redirect } from "next/navigation";
 import "../../dashboard/dashboard.css"; 
 import SetNewPassword from '@/components/supabase-auth/SetNewPassword';
 import DashboardMenu from "@/components/dashboard/DashboardMenu";
 
 export default async function SetNewPasswordPage() {
-  const supabase = createClient();
-  const { data: { user } } = await supabase.auth.getUser();
-
-  if (!user) {
-    return redirect("/login");
-  }
 
   return (
     <div className="dashboard-layout">
