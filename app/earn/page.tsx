8a873e4b91867acbb9fd298f5f9c3fa4e008edf1
SolomonFoskaay <oskfak@gmail.com>
2024-05-03 19:18:29 +0000

    Updated Earn page content 1

--- app/earn/page.tsx
+++ app/earn/page.tsx
@@ -39,7 +39,8 @@ export default function EarnPage() {
         B. EARN PATHS OFFERED BY EXPLORESOL:
       </h2>
       <p className="text-md mb-4 text-justify">
-        See the existing earning paths and amount of dPUT reward each can earn you on ExploreSol platform below:
+        See the existing earning paths and amount of dPUT reward each can earn
+        you on ExploreSol platform below:
       </p>
       <div>
         {problemsAndSolutions.map((item, index) => (
@@ -74,19 +75,72 @@ export default function EarnPage() {
         ))}
       </div>
 
+      {/* Earning rewards calculations Section */}
+      <div className="my-6">
+        <h2 className="text-2xl font-bold my-6 text-purple-500">
+          C. dPUT REWARD TOKEN CALCULATION:
+        </h2>
+        <p className="text-md mb-4 text-justify">
+          Considering the dPUT used for reward is pegged to SOL not stable coin
+          like USDT/USDC, The SOL is pegged to USDT.
+        </p>
+        <p className="text-md mb-4 text-justify">
+          So, for example, today's SOL price is $140, then the dPUT reward is
+          calculated as follows:
+        </p>
+        <p className="text-md mb-4 text-justify">
+          $2(USDT) = 0.014289027SOL = 400,000 dPUT (dPUT amount is based on
+          current exchange rate to SOL)
+        </p>
+        <p className="text-md mb-4 text-justify">
+          dPUT amount you get per reward payment cycle changes from time to time
+          to reflect the current exchange rate of dPU to SOL at time of the
+          reward payment. it may be more or less.
+        </p>
+      </div>
+
+      {/* Earning rewards payment Section */}
+      <div className="my-6">
+        <h2 className="text-2xl font-bold my-6 text-purple-500">
+          D. EXPLORESOL dPUT PAYMENT CYCLE:
+        </h2>
+        <p className="text-md mb-4 text-justify">
+          The dPUT earned reward payment is done on weekly bases every Monday.
+        </p>
+        <p className="text-md mb-4 text-justify">
+          You get payment for the previous week (Monday to Sunday)
+        </p>
+        <p className="text-md mb-4 text-justify">Minimum Payment Threshold?</p>
+        <p className="text-md mb-4 text-justify">
+          No minimum threshold, you get your dPUT sent to your wallet on
+          ExploreSol.xyz based on the amount you have earn through the week.
+        </p>
+        <p className="text-md mb-4 text-justify">
+          For example if you get 5 Solana projects listings approved within 1
+          week. When payment for the week will be done, you get dPUT for all the 5
+          approved listings.
+        </p>
+        <p className="text-md mb-4 text-justify">
+          If you don't have any listing approved in the week, you will get 0
+          dPUT.
+        </p>
+      </div>
+
       {/* Earning Opportunities Section */}
       <div className="my-6">
         <h2 className="text-2xl font-bold my-6 text-purple-500">
-          C. WHAT IS EXPLORESOL dPUT REWARD TOKEN:
+          E. WHAT IS EXPLORESOL dPUT REWARD TOKEN:
         </h2>
-        <p>
+        <p className="text-md mb-4 text-justify">
           The dPUT earned is the official utility token of dProgramming
           University (dPU) ecosystem and ExploreSol is one of the project in the
           dPU ecosystem that qualify to reward users with dPUT.
         </p>
-        <p>Want to learn more and how to exchange your earned dPUT for SOL?</p>
-        <p>Then check out the: </p>
-        <p>
+        <p className="text-md mb-4 text-justify">
+          Want to learn more and how to exchange your earned dPUT for SOL?
+        </p>
+        <p className="text-md mb-4 text-justify">Then check out the: </p>
+        <p className="text-md mb-4 text-justify">
           <Link
             href="https://dProgrammingUniversity.com/dPUT"
             target="_blank"
@@ -106,26 +160,26 @@ const problemsAndSolutions = [
     title: "Earn Path 1: Submit Listings",
     description:
       "Earn dPUT (utility token) reward for approved project submissions.",
-    reward: "1,000dPUT",
+    reward: "$2(USDT)SOL worth of dPUT (see section C below for details)",
   },
   {
     title: "Earn Path 2: Participate in Social Campaigns",
     description:
       "Earn dPUT (utility token) reward for engaging with project-specific campaigns.",
-    reward: "1,000dPUT",
+    reward: "$1(USDT)SOL worth of dPUT (see section C below for details)",
   },
   {
     title: "Earn Path 3: Referral Program",
     description:
       "Earn dPUT (utility token) reward for referring others to subscribe to the ExploreSol 'S' Newsletter..",
-    reward: "1,000dPUT",
+    reward: "$0.10(USDT)SOL worth of dPUT (see section C below for details)",
   },
   {
     title: "Earn Path 4: Stay Tuned for More",
     description:
       "Earn dPUT (utility token) reward for Future opportunities are on the horizon, ensuring our community remains vibrant and rewarded.",
     reward:
-      "20%(200Million) of dPUT 1Billion Max Supply is planned towards community rewards for users and contributors to Solana projects across dProgramming University (dPU) ecosystem",
+      "More dPUT is planned towards community rewards for users and contributors to Solana projects across dProgramming University (dPU) ecosystem",
   },
   // More earning paths and amount
 ];
