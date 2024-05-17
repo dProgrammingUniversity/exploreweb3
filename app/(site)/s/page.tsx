5cd8c9e41373a77b5fe620f53880d43ff9cb6330
SolomonFoskaay <oskfak@gmail.com>
2024-05-17 20:06:00 +0000

    Added S page

--- app/(site)/s/page.tsx
+++ app/(site)/s/page.tsx
@@ -1,62 +1,64 @@
-// ExploreSol/app/s/page.tsx
-"use client";
-import { useState, useEffect } from "react";
+// /app/(site)/s/page.tsx
+import S from "@/components/S";
+import { Metadata } from "next";
+import Image from "next/image";
 import Link from "next/link";
-import EmailSubscriptionForm from "@/components/EmailSubscriptionForm";
 
-export default function AboutPage() {
-  const [isOpen, setIsOpen] = useState(Array(6).fill(false));
-  // Define fixed metadata values
-  const title = "S - ExploreSol";
-  const description =
-    "Earn while Exploring the best and most interesting Solana Projects.";
-  const ogImage =
-    "https://res.cloudinary.com/difhad1rl/image/upload/v1712648696/ExploreSol-Banner-01_qgtopx.jpg";
+export const metadata: Metadata = {
+  title: "Earn - Explore Solana",
+  description: "Earn while exploring Solana projects",
+  // other metadata
+};
 
-  useEffect(() => {
-    // Helper function to update or create meta tags
-    const updateMetaTag = (name: string, content: string, property = false) => {
-      let selector = property
-        ? `meta[property="${name}"]`
-        : `meta[name="${name}"]`;
-      let metaTag = document.querySelector(selector);
-      if (!metaTag) {
-        metaTag = document.createElement("meta");
-        if (property) {
-          metaTag.setAttribute("property", name);
-        } else {
-          metaTag.setAttribute("name", name);
-        }
-        document.getElementsByTagName("head")[0].appendChild(metaTag);
-      }
-      metaTag.setAttribute("content", content);
-    };
-
-    // Update the document title and meta tags
-    document.title = title; // Update the title
-    updateMetaTag("description", description);
-    updateMetaTag("og:title", title, true);
-    updateMetaTag("og:description", description, true);
-    updateMetaTag("og:image", ogImage, true);
-    updateMetaTag("og:url", window.location.href, true);
-    updateMetaTag("og:type", "website", true);
-  }, []); // Empty dependency array to run only once on component mount
-
-  // Toggle selection function
-  const toggleSection = (index: number) => {
-    setIsOpen(isOpen.map((open, i) => (i === index ? !open : false)));
-  };
-
+const EarnPage = () => {
   return (
-    <div className="container mx-auto px-4">
-      <h1 className="text-2xl font-bold text-center my-6 text-purple-500">
-        S WORDS COMMUNITY
-      </h1>
-      <p>Watchout for the S words community.</p>
-      <EmailSubscriptionForm />
-      <p>
-        <Link href="/">Back to home</Link>
-      </p>
-    </div>
+    <>
+    <section className="flex-grow pb-12.5 pt-32.5 lg:pb-25 lg:pt-45 xl:pb-30 xl:pt-50">
+      <div className="relative z-1 mx-auto max-w-c-1016 px-7.5 pb-7.5 pt-10 lg:px-15 lg:pt-15 xl:px-20 xl:pt-20">
+        <div className="absolute left-0 top-0 -z-1 h-2/3 w-full rounded-lg bg-gradient-to-t from-transparent to-[#dee7ff47] dark:bg-gradient-to-t dark:to-[#252A42]">
+          <div className="absolute bottom-17.5 left-0 -z-1 h-1/3 w-full">
+            <Image
+              src="/images/shape/shape-dotted-light.svg"
+              alt="Dotted"
+              className="dark:hidden"
+              fill
+            />
+            <Image
+              src="/images/shape/shape-dotted-dark.svg"
+              alt="Dotted"
+              className="hidden dark:block"
+              fill
+            />
+          </div>
+
+          <div className="flex flex-col items-center justify-center">
+            <Link
+              href="/"
+              className="text-foreground bg-btn-background hover:bg-btn-background-hover group absolute left-8 top-8 flex items-center rounded-md px-4 py-2 text-sm no-underline"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                className="mr-2 h-4 w-4 transition-transform group-hover:-translate-x-1"
+              >
+                <polyline points="15 18 9 12 15 6" />
+              </svg>{" "}
+              Back
+            </Link>
+          </div>
+        </div>
+            <S />
+      </div>
+    </section>
+    </>
   );
-}
+};
+
+export default EarnPage;
---
+++ components/S/index.tsx
@@ -0,0 +1,19 @@
+// /components/S/index.tsx
+import EmailSubscriptionForm from "../Directory/Newsletter/EmailSubscriptionForm";
+
+const S = () => {
+
+  return (
+    <>
+      <h1 className="text-2xl font-bold text-center my-6 text-purple-500">
+        S WORDS COMMUNITY
+      </h1>
+      <p>Watchout for the S words community.</p>
+      <EmailSubscriptionForm />
+    </>
+  );
+};
+
+
+
+export default S;
