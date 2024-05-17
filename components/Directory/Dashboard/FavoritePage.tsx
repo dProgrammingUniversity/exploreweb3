543ff21839f932dc429dfb8317472b0bcc671ddf
SolomonFoskaay <oskfak@gmail.com>
2024-05-17 21:35:59 +0000

    Fixed favorites page

--- components/Directory/Dashboard/FavoritePage.tsx
+++ components/Directory/Dashboard/FavoritePage.tsx
@@ -1,13 +1,15 @@
-// Exploresol/components/dashboard/FavoritePage.tsx
+// /components/Directory/Dashboard/FavoritePage.tsx
 "use client";
-import React, { useEffect, useState } from 'react';
-import { createClient } from '@/utils/supabase/client';
-import ListingsCard from '@/components/ListingsCard';
-import "../../app/dashboard/dashboard.css"; 
+import React, { useEffect, useState } from "react";
+import { createClient } from "@/utils/supabase/client";
+import ListingsCard from "../Listings/ListingsCard";
+import Pagination from "@/components/Pagination"; // Ensure this import path is correct based on your project structure
 
 const FavoritePage = () => {
   const [favoriteListings, setFavoriteListings] = useState<DisplayListingTypes[]>([]);
   const [userId, setUserId] = useState<string | null>(null);
+  const [currentPage, setCurrentPage] = useState(1);
+  const itemsPerPage = 9; // Adjust if necessary
   const supabaseClient = createClient();
 
   useEffect(() => {
@@ -25,8 +27,8 @@ const FavoritePage = () => {
   useEffect(() => {
     const fetchFavorites = async () => {
       if (!userId) return; // Ensure userId is not null
-      const { data, error } = await supabaseClient
-        .rpc('favorites_fetch_all_at_once', { user_id: userId });
+
+      const { data, error } = await supabaseClient.rpc("favorites_fetch_all_at_once", { user_id: userId });
 
       if (!error && data) {
         setFavoriteListings(data);
@@ -36,11 +38,26 @@ const FavoritePage = () => {
     fetchFavorites();
   }, [userId]);
 
+  // Calculate the number of pages
+  const totalPages = Math.ceil(favoriteListings.length / itemsPerPage);
+
+  // Get current listings to display
+  const indexOfLastListing = currentPage * itemsPerPage;
+  const indexOfFirstListing = indexOfLastListing - itemsPerPage;
+  const currentListings = favoriteListings.slice(indexOfFirstListing, indexOfLastListing);
+
   return (
-    <div className='listings-card-grid-display'>
-      {favoriteListings.map(listing => (
-        <ListingsCard key={listing.id} listing={listing} />
-      ))}
+    <div>
+      <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
+        {currentListings.map((listing) => (
+          <ListingsCard key={listing.id} listing={listing} />
+        ))}
+      </div>
+      <Pagination
+        currentPage={currentPage}
+        totalPages={totalPages}
+        onPageChange={setCurrentPage}
+      />
     </div>
   );
 };
