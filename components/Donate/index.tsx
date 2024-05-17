51ebe40b24b384fd4733d60e3e7c6e6d17f3890c
SolomonFoskaay <oskfak@gmail.com>
2024-05-17 21:05:37 +0000

    Fixed wallet display on mobile and copy button

--- components/Donate/index.tsx
+++ components/Donate/index.tsx
@@ -1,13 +1,12 @@
 // /components/Donate/index.tsx
-"use client"
+"use client";
 import React, { useState } from 'react';
 
 const Donate = () => {
   const [walletAddress] = useState('63t6dZ78VFW1yX7uFJV678qFbZweDpUDenjLYEsq2J7q');
-  
+
   const copyToClipboard = async () => {
     if (navigator.clipboard && window.isSecureContext) {
-      // Try to use the Clipboard API
       try {
         await navigator.clipboard.writeText(walletAddress);
         alert('Wallet address copied to clipboard!');
@@ -16,13 +15,11 @@ const Donate = () => {
         fallbackCopyTextToClipboard(walletAddress);
       }
     } else {
-      // Fallback if Clipboard API is not available
       fallbackCopyTextToClipboard(walletAddress);
     }
   };
 
   const fallbackCopyTextToClipboard = (text: string) => {
-    // Create a text area element dynamically
     const textArea = document.createElement('textarea');
     textArea.value = text;
     document.body.appendChild(textArea);
@@ -45,21 +42,19 @@ const Donate = () => {
   return (
     <>
       <h1 className="text-2xl font-bold text-center my-6 text-purple-400">
-        Kindly Donate to Keep ExploreSol Project Going
+        Kindly Donate to Keep ExploreSolana Project Going
       </h1>
       <p className="text-md mb-4">
         You can support by donating SOL and SOL tokens to the following wallet address - THANKS!:
       </p>
-      <p className="input-address" style={{ userSelect: 'text' }}>
+      <p className="break-words text-lg mb-2 p-2 border rounded-md bg-gray-800 text-white" style={{ userSelect: 'text' }}>
         {walletAddress}
       </p>
-      <button onClick={copyToClipboard} className="btn mt-2">
+      <button onClick={copyToClipboard} className="mt-2 px-4 py-2 bg-blue-500 text-white font-semibold rounded-md shadow-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75 transition ease-in-out duration-300">
         Copy Wallet Address
       </button>
     </>
   );
 };
-
-
 
 export default Donate;
