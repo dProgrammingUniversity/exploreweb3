f6c4ed181b4e65699e0e5a95ab88006ae86e91ff
SolomonFoskaay <oskfak@gmail.com>
2024-05-23 14:35:31 +0000

    Updated Hero with animated Solana map

--- components/Hero/index.tsx
+++ components/Hero/index.tsx
@@ -1,63 +1,43 @@
 // /components/Hero/index.tsx
 "use client";
 import Image from "next/image";
-import { useState } from "react";
-import EmailSubscriptionForm from "../Directory/Newsletter/EmailSubscriptionForm";
+import { motion } from "framer-motion";
 
 const Hero = () => {
-
   return (
     <>
       <section className="overflow-hidden pb-20 pt-35 md:pt-40 xl:pb-25 xl:pt-46">
         <div className="mx-auto max-w-c-1390 px-4 md:px-8 2xl:px-0">
           
-          {/* Left column section */}
-          <div className="flex lg:items-center lg:gap-8 xl:gap-32.5">
-            <div className=" md:w-1/2">
-              <div className="mt-10">
-                {/* Newsletter subscription */}
-                <EmailSubscriptionForm />
-              </div>
-            </div>
-
-            {/* Right column section */}
-            <div className="animate_right hidden md:w-1/2 lg:block">
+          {/* Full-width section */}
+          <div className="flex justify-center">
+            <div className="animate_right w-full lg:block">
               <div className="relative 2xl:-mr-7.5">
-                <Image
-                  src="/images/shape/shape-01.png"
-                  alt="shape"
-                  width={46}
-                  height={246}
-                  className="absolute -left-11.5 top-0"
-                />
-                <Image
-                  src="/images/shape/shape-02.svg"
-                  alt="shape"
-                  width={36.9}
-                  height={36.7}
-                  className="absolute bottom-0 right-0 z-10"
-                />
-                <Image
-                  src="/images/shape/shape-03.svg"
-                  alt="shape"
-                  width={21.64}
-                  height={21.66}
-                  className="absolute -right-6.5 bottom-0 z-1"
-                />
-                <div className=" relative aspect-[700/444] w-full">
+                <motion.div
+                  className="relative aspect-[700/444] w-full"
+                  initial={{ y: 0, rotate: 0 }}
+                  animate={{ y: [0, -10, 0], rotate: [0, 5, -5, 0] }}
+                  transition={{
+                    duration: 8,
+                    ease: "easeInOut",
+                    repeat: Infinity,
+                    repeatType: "loop"
+                  }}
+                  whileHover={{ scale: 1.05 }}
+                >
                   <Image
                     className="shadow-solid-l dark:hidden"
-                    src="/images/hero/hero-light.svg"
-                    alt="Hero"
+                    src="/images/hero/ExploreSolana-Banner-03a2-Website-Banner-Light-Transparent-bg.png"
+                    alt="Solana Ecosystem Map"
                     fill
                   />
                   <Image
                     className="hidden shadow-solid-l dark:block"
-                    src="/images/hero/hero-dark.svg"
-                    alt="Hero"
+                    src="/images/hero/ExploreSolana-Banner-03a2-Website-Banner-Light-Transparent-bg.png"
+                    alt="Solana Ecosystem Map"
                     fill
                   />
-                </div>
+                </motion.div>
               </div>
             </div>
           </div>
