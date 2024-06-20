514e3fe14a4fa68c7b1d1fb0298194e5e6e26327
SolomonFoskaay <oskfak@gmail.com>
2024-06-20 10:01:54 +0000

    Updated Donate page

--- components/Donate/index.tsx
+++ components/Donate/index.tsx
@@ -1,18 +1,20 @@
 // /components/Donate/index.tsx
 "use client";
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const Donate = () => {
-  const [walletAddress] = useState('63t6dZ78VFW1yX7uFJV678qFbZweDpUDenjLYEsq2J7q');
+  const [walletAddress] = useState(
+    "63t6dZ78VFW1yX7uFJV678qFbZweDpUDenjLYEsq2J7q",
+  );
 
   // Wallet copy function starts
   const copyToClipboard = async () => {
     if (navigator.clipboard && window.isSecureContext) {
       try {
         await navigator.clipboard.writeText(walletAddress);
-        alert('Wallet address copied to clipboard!');
+        alert("Wallet address copied to clipboard!");
       } catch (err) {
-        console.error('Copy failed:', err);
+        console.error("Copy failed:", err);
         fallbackCopyTextToClipboard(walletAddress);
       }
     } else {
@@ -21,20 +23,20 @@ const Donate = () => {
   };
 
   const fallbackCopyTextToClipboard = (text: string) => {
-    const textArea = document.createElement('textarea');
+    const textArea = document.createElement("textarea");
     textArea.value = text;
     document.body.appendChild(textArea);
     textArea.focus();
     textArea.select();
 
     try {
-      const successful = document.execCommand('copy');
-      const msg = successful ? 'successful' : 'unsuccessful';
-      console.log('Fallback: Copying text command was ' + msg);
-      alert('Wallet address copied to clipboard!');
+      const successful = document.execCommand("copy");
+      const msg = successful ? "successful" : "unsuccessful";
+      console.log("Fallback: Copying text command was " + msg);
+      alert("Wallet address copied to clipboard!");
     } catch (err) {
-      console.error('Fallback: Oops, unable to copy', err);
-      alert('Failed to copy wallet address. Please try manually.');
+      console.error("Fallback: Oops, unable to copy", err);
+      alert("Failed to copy wallet address. Please try manually.");
     }
 
     document.body.removeChild(textArea);
@@ -43,18 +45,31 @@ const Donate = () => {
 
   return (
     <>
-      <h1 className="text-2xl font-bold text-center my-6 text-purple-400">
+      <h1 className="my-6 text-center text-2xl font-bold text-purple-400">
         Kindly Donate to Keep ExploreSolana Project Going
       </h1>
       <p className="text-md mb-4">
-        You can support by donating SOL and SOL tokens to the following wallet address - THANKS!:
+        You can support by donating SOL and SOL tokens to the following wallet
+        address - THANKS!:
       </p>
-      <p className="break-words text-lg mb-2 p-2 border rounded-md bg-gray-800 text-white" style={{ userSelect: 'text' }}>
+      <p
+        className="mb-2 break-words rounded-md border bg-gray-800 p-2 text-lg text-white"
+        style={{ userSelect: "text" }}
+      >
         {walletAddress}
       </p>
-      <button onClick={copyToClipboard} className="mt-2 px-4 py-2 bg-blue-500 text-white font-semibold rounded-md shadow-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75 transition ease-in-out duration-300">
+      <button
+        onClick={copyToClipboard}
+        className="mb-5 mt-2 rounded-md bg-blue-500 px-4 py-2 font-semibold text-white shadow-md transition duration-300 ease-in-out hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75"
+      >
         Copy Wallet Address
       </button>
+      <p className="text-md mb-4">
+        Thanks! 
+        <br />
+        NOTE: All donation wallets will qualify for future appreciation
+        (hint - WL😀)!
+      </p>
     </>
   );
 };
