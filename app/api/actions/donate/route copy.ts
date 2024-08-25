b5d1b31fdee6e1a8d423faedab17434a58f71370
SolomonFoskaay <oskfak@gmail.com>
2024-08-25 12:49:59 +0000

    Redesign Blinks game with new features 3

--- app/api/actions/donate/route copy.ts
+++ app/api/actions/donate/route copy.ts
@@ -5,6 +5,7 @@ import {
   ActionPostRequest,
   createPostResponse,
   ActionPostResponse,
+  MEMO_PROGRAM_ID
 } from "@solana/actions";
 import {
   clusterApiUrl,
@@ -13,43 +14,60 @@ import {
   PublicKey,
   SystemProgram,
   Transaction,
+  TransactionInstruction,
 } from "@solana/web3.js";
 import { BlinksightsClient } from 'blinksights-sdk';
+
 
 // Initialize Blinksights client
 const client = new BlinksightsClient(`${process.env.NEXT_PUBLIC_BLINKSIGHTS_API_KEY}`);
 
 export const GET = async (req: Request) => {
   const payload: ActionGetResponse = {
-    title: "ExploreWeb3 - Buy Me a Coffee",
-    icon: new URL(
-        "/images/blinks/ExploreWeb3-Quiz-Game-image-01b-WITH-LOGO.jpg",
-        new URL(req.url).origin,
-      ).toString(),
+    title: "Support Explore Web3 - Kindly DONATE To Keep This Project Going!",
+    icon: "https://res.cloudinary.com/difhad1rl/image/upload/v1720684490/ExploreSolana-Banner-03a5a-Website-Banner-Light-Transparent-bg-Animated-1024x1024.gif",
     label: "Donate",
     description:
-      "Buy me a coffee with SOL using this super sweet blink of mine :)",
+      `Explore Web3 platform is a PUBLIC GOOD project that Showcases and Promotes Amazing Web3 Ecosystems Projects for FREE!
+
+Featuring SOLANA Ecosystem Projects and Blinks for FREE for easy discovery by new/existing Solana ecosystem users/the world.
+
+However, it may not last long due to the lack of funding. Thus, Kindly consider DONATING to keep it on and promoting Web3 Projects for FREE!
+
+NOTE:- All donation wallets will qualify for future appreciation (hint - WL😀)!!!`,
     links: {
       actions: [
         {
-          href: "/api/actions/donate?amount=0.001",
-          label: "0.001 SOL",
+          href: "/api/actions/donate?amount=0.1&note=SmallCoffee",
+          label: "0.1 SOL",
         },
         {
-          href: "/api/actions/donate?amount=0.5",
+          href: "/api/actions/donate?amount=0.5&note=BigCoffee",
           label: "0.5 SOL",
         },
         {
-          href: "/api/actions/donate?amount=1.0",
+          href: "/api/actions/donate?amount=1.0&note=CoffeeFor1Week",
           label: "1.0 SOL",
         },
         {
-          href: "/api/actions/donate?amount={amount}",
-          label: "Send SOL",
+          href: "/api/actions/donate?amount=3.0&note=CoffeeFor2Week",
+          label: "3 SOL",
+        },
+        {
+          href: "/api/actions/donate?amount=5.0&note=CoffeeFor3Week",
+          label: "5 SOL",
+        },
+        {
+          href: "/api/actions/donate?amount=10.0&note=CoffeeFor1Month",
+          label: "10 SOL",
+        },
+        {
+          href: "/api/actions/donate?amount={amount}&note=CustomAmountCoffee",
+          label: "Donate",
           parameters: [
             {
               name: "amount",
-              label: "Enter a SOL amount",
+              label: "Enter a custom SOL amount",
             },
           ],
         },
@@ -79,14 +97,19 @@ export const POST = async (req: Request) => {
       throw "Invalid 'account' provided. It is not a real pubkey.";
     }
 
-    let amount: number = 0;
+    let amount: number = 2;
+    let note: string = "No Note Provided";
 
     if (url.searchParams.has("amount")) {
       try {
-        amount = parseFloat(url.searchParams.get("amount") || "0.000051") || amount;
+        amount = parseFloat(url.searchParams.get("amount") || "1.5") || amount;
       } catch (err) {
         throw "Invalid 'amount' input.";
       }
+    }
+
+    if (url.searchParams.has("note")) {
+      note = url.searchParams.get("note") || note;
     }
 
     const connection = new Connection(clusterApiUrl("mainnet-beta"));
@@ -101,6 +124,11 @@ export const POST = async (req: Request) => {
         lamports: amount * LAMPORTS_PER_SOL,
         toPubkey: TO_PUBKEY,
       }),
+      new TransactionInstruction({
+        keys: [],
+        data: Buffer.from(note, "utf-8"),
+        programId: new PublicKey(MEMO_PROGRAM_ID),
+      }),
     );
     transaction.feePayer = account;
     transaction.recentBlockhash = (
@@ -110,7 +138,7 @@ export const POST = async (req: Request) => {
     const payload: ActionPostResponse = await createPostResponse({
       fields: {
         transaction,
-        message: "Thanks for the coffee fren :)",
+        message: "Thanks for the support donation and the note!!! :)",
       },
     });
 
@@ -134,4 +162,3 @@ export const POST = async (req: Request) => {
     );
   }
 };
-
