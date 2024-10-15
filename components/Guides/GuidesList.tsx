db17dd88a355df1f08c793a7490e26a79906a719
SolomonFoskaay <oskfak@gmail.com>
2024-10-15 10:47:39 +0000

    Guides fixes 3

--- components/Guides/GuidesList.tsx
+++ components/Guides/GuidesList.tsx
@@ -1,17 +1,23 @@
 // /components/Guides/GuidesList.tsx
-const GuideList = ({ projectName, guide }) => {
-    return (
-      <aside className="w-full lg:w-1/4 p-4">
-        <h2 className="text-xl font-bold">{projectName}</h2>
-        <ul>
-          <li>
-            <a href={`/guides/${guide.slug}`} className="text-blue-500">
-              {guide.short_title}
-            </a>
-          </li>
-        </ul>
-      </aside>
-    );
-  };
-  
-  export default GuideList;
+const GuideList = ({ projectsMap }) => {
+  return (
+    <aside className="w-full lg:w-1/4 p-4">
+      {Object.entries(projectsMap).map(([projectName, guides]) => (
+        <div key={projectName}>
+          <h2 className="text-xl font-bold">{projectName}</h2>
+          <ul>
+            {Array.isArray(guides) && guides.map((guide) => (
+              <li key={guide.slug}>
+                <a href={`/guides/${guide.slug}`} className="text-blue-500">
+                  {guide.short_title}
+                </a>
+              </li>
+            ))}
+          </ul>
+        </div>
+      ))}
+    </aside>
+  );
+};
+
+export default GuideList;
